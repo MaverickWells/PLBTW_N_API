a2636815b6b000ade08931d20400c5c1789de3a4
Welly Wells <wellyngg@gmail.com>
2016-11-07 07:28:18 +0700

    add login api

---
+++ application/controllers/api/login.php
@@ -0,0 +1,28 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH.'/libraries/REST_Controller.php';
+
+class LOGIN extends REST_Controller
+{
+	function user_get()
+    {
+    	// $this->load->model('db_model');
+        $data = $this->db_model->CheckLoginData($this->get('username'), $this->get('password'));
+
+        if($data)
+            $this->response(array('status' => $this->get('username')), 200); // 200 being the HTTP response code
+        else
+            $this->response(array('error' => 'Wrong Username or Password'), 200);
+    }
+
+    // function user_post()
+    // {
+    // 	// $this->load->model('db_model');
+    //     $data = $this->db_model->CheckLoginData($this->get('username'), $this->get('password'));
+
+    //     //if($data)
+    //         $this->response(array('status' => $this->input->post('username')), 200); // 200 being the HTTP response code
+    //     //else
+    //        // $this->response(array('error' => 'Wrong Username or Password'), 200);
+    // }
+}
---
+++ application/models/db_model.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class DB_MODEL extends CI_Model {
+	public function CheckLoginData($username, $password)
+	{
+		# code...
+		$link = mysqli_connect("localhost", "root", "", "plbtw");
+		$usr = mysqli_real_escape_string($link, $username);
+		$pwd = md5(mysqli_real_escape_string($link, $password));
+
+		$login_check = $this->db->get_where('user', array('username' => $usr, 'password' => $pwd));
+		if($login_check->num_rows() > 0){
+			$rows = $login_check->row();			
+			if($usr == $rows->username && $pwd == $rows->password){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+	}
+}
