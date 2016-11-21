6523e996aa570993dad110407eb2345aa9875bcf
Welly Wells <wellyngg@gmail.com>
2016-11-21 17:53:32 +0700

    add api key checker model add api key check for every method before checking another thing

--- application/controllers/api/news.php
+++ application/controllers/api/news.php
@@ -4,41 +4,25 @@ require APPPATH.'/libraries/REST_Controller.php';
 
 class NEWS extends REST_Controller
 {
-	// function login_get()
-    // {
-    // 	// $this->load->model('db_model');
-    //     $data = $this->db_model->CheckLoginData($this->get('username'), $this->get('password'));
-	//
-    //     if($data)
-    //         $this->response(array('status_code' => '200', 'result' => $data), 200); // 200 being the HTTP response code
-    //     else
-    //         $this->response(array('error' => 'Wrong Username or Password'), 200);
-    // }
-	//
-    // function login_post()
-    // {
-    // 	// $this->load->model('db_model');
-    //     $data = $this->db_model->CheckLoginData($this->post('username'), $this->post('password'));
-	//
-    //     if($data)
-    //         $this->response(array('status_code' => '200', 'result' => $data), 200); // 200 being the HTTP response code
-    //     else
-    //        $this->response(array('error' => 'Wrong Username or Password'), 200);
-    // }
-
     function all_news_get()
     {
-	        $data = $this->news_model->GetAllNews();
+        if($this->api_model->CheckAPIKEY($this->post('api_key')) > 0){
+            $data = $this->news_model->GetAllNews();
 
 	        if($data)
 	            $this->response(array('status_code' => '200', 'result' => $data), 200); // 200 being the HTTP response code
 	        else
 	            $this->response(array('error' => 'Wrong Username or Password'), 200);
+		}
+		else {
+			$this->response(array('result' => 'No API KEY Provided'), 401);
+		}
     }
 
 	function news_get($id)
     {
-	        $data = $this->news_model->GetNews($id);
+        if($this->api_model->CheckAPIKEY($this->post('api_key')) > 0){
+            $data = $this->news_model->GetNews($id);
 
 	        if($data)
 	            $this->response(array('status_code' => '200', 'result' => $data), 200); // 200 being the HTTP response code
@@ -47,10 +31,15 @@ class NEWS extends REST_Controller
 			else {
 				$this->response(array('error' => 'Incorrect ID or Other Error'), 200);
 			}
+		}
+		else {
+			$this->response(array('result' => 'No API KEY Provided'), 401);
+		}
     }
 
     function news_location_get($location)
     {
+        if($this->api_model->CheckAPIKEY($this->post('api_key')) > 0){
 	        $data = $this->news_model->GetLocationNews($location);
 
 	        if($data)
@@ -60,98 +49,117 @@ class NEWS extends REST_Controller
 			else {
 				$this->response(array('error' => 'Incorrect ID or Other Error'), 200);
 			}
+		}
+		else {
+			$this->response(array('result' => 'No API KEY Provided'), 401);
+		}
     }
 
     function news_post()
     {
-        $link = mysqli_connect("localhost", "root", "root", "plbtw");
+        if($this->api_model->CheckAPIKEY($this->post('api_key')) > 0){
+            $link = mysqli_connect("localhost", "root", "root", "plbtw");
 
-        $data = array(
-			'title' => mysqli_real_escape_string($link, $this->input->post('title')),
-			'content' => mysqli_real_escape_string($link, $this->input->post('content')),
-			'category' => mysqli_real_escape_string($link, $this->input->post('category')),
-			'sub_category' => mysqli_real_escape_string($link, $this->input->post('sub_category')),
-			'location' => mysqli_real_escape_string($link, $this->input->post('location')),
-			'news_web' => mysqli_real_escape_string($link, $this->input->post('news_web')),
-			'news_url' => mysqli_real_escape_string($link, $this->input->post('news_url')),
-			'keyword' => mysqli_real_escape_string($link, $this->input->post('keyword')),
-        );
-
-        $result = $this->news_model->CreateNews($data);
-
-        if($result > 0){
-            $this->response(array(
-                'result' => 'Successful Insertion',
-                'affected_rows' => $result,
-				'news_id' => $this->db->insert_id(),
-                'status_code' => 200
-                ), REST_Controller::HTTP_OK
+            $data = array(
+    			'title' => mysqli_real_escape_string($link, $this->input->post('title')),
+    			'content' => mysqli_real_escape_string($link, $this->input->post('content')),
+    			'category' => mysqli_real_escape_string($link, $this->input->post('category')),
+    			'sub_category' => mysqli_real_escape_string($link, $this->input->post('sub_category')),
+    			'location' => mysqli_real_escape_string($link, $this->input->post('location')),
+    			'news_web' => mysqli_real_escape_string($link, $this->input->post('news_web')),
+    			'news_url' => mysqli_real_escape_string($link, $this->input->post('news_url')),
+    			'keyword' => mysqli_real_escape_string($link, $this->input->post('keyword')),
             );
-        }
-        else{
-            $this->response(array(
-                'result' => 'Failed Insertion',
-                'affected_rows' => $result,
-                'status_code' => 200
-                ), REST_Controller::HTTP_OK
-            );
-        }
+
+            $result = $this->news_model->CreateNews($data);
+
+            if($result > 0){
+                $this->response(array(
+                    'result' => 'Successful Insertion',
+                    'affected_rows' => $result,
+    				'news_id' => $this->db->insert_id(),
+                    'status_code' => 200
+                    ), REST_Controller::HTTP_OK
+                );
+            }
+            else{
+                $this->response(array(
+                    'result' => 'Failed Insertion',
+                    'affected_rows' => $result,
+                    'status_code' => 200
+                    ), REST_Controller::HTTP_OK
+                );
+            }
+		}
+		else {
+			$this->response(array('result' => 'No API KEY Provided'), 401);
+		}
     }
 
     function news_put($idnews){
-        $link = mysqli_connect("localhost", "root", "root", "plbtw");
+        if($this->api_model->CheckAPIKEY($this->post('api_key')) > 0){
+            $link = mysqli_connect("localhost", "root", "root", "plbtw");
 
-        parse_str(file_get_contents("php://input"),$put_vars);
+            parse_str(file_get_contents("php://input"),$put_vars);
 
-		$data = array(
-			'title' => mysqli_real_escape_string($link, $put_vars['title']),
-			'content' => mysqli_real_escape_string($link, $put_vars['content']),
-			'category' => mysqli_real_escape_string($link, $put_vars['category']),
-			'sub_category' => mysqli_real_escape_string($link, $put_vars['sub_category']),
-			'location' => mysqli_real_escape_string($link, $put_vars['location']),
-			'news_web' => mysqli_real_escape_string($link, $put_vars['news_web']),
-			'news_url' => mysqli_real_escape_string($link, $put_vars['news_url']),
-			'keyword' => mysqli_real_escape_string($link, $put_vars['keyword']),
-        );
-
-        $result = $this->news_model->UpdateNews($data, $idnews);
-
-        if($result > 0){
-            $this->response(array(
-                'result' => 'Successful Update',
-                'affected_rows' => $result,
-                'status_code' => 200
-                ), REST_Controller::HTTP_OK
+    		$data = array(
+    			'title' => mysqli_real_escape_string($link, $put_vars['title']),
+    			'content' => mysqli_real_escape_string($link, $put_vars['content']),
+    			'category' => mysqli_real_escape_string($link, $put_vars['category']),
+    			'sub_category' => mysqli_real_escape_string($link, $put_vars['sub_category']),
+    			'location' => mysqli_real_escape_string($link, $put_vars['location']),
+    			'news_web' => mysqli_real_escape_string($link, $put_vars['news_web']),
+    			'news_url' => mysqli_real_escape_string($link, $put_vars['news_url']),
+    			'keyword' => mysqli_real_escape_string($link, $put_vars['keyword']),
             );
-        }
-        else{
-            $this->response(array(
-                'result' => 'Failed Update',
-                'affected_rows' => $result,
-                'status_code' => 200
-                ), REST_Controller::HTTP_OK
-            );
-        }
+
+            $result = $this->news_model->UpdateNews($data, $idnews);
+
+            if($result > 0){
+                $this->response(array(
+                    'result' => 'Successful Update',
+                    'affected_rows' => $result,
+                    'status_code' => 200
+                    ), REST_Controller::HTTP_OK
+                );
+            }
+            else{
+                $this->response(array(
+                    'result' => 'Failed Update',
+                    'affected_rows' => $result,
+                    'status_code' => 200
+                    ), REST_Controller::HTTP_OK
+                );
+            }
+		}
+		else {
+			$this->response(array('result' => 'No API KEY Provided'), 401);
+		}
     }
 
     function news_delete($idnews){
-        $result = $this->news_model->DeleteNews($idnews);
+        if($this->api_model->CheckAPIKEY($this->post('api_key')) > 0){
+            $result = $this->news_model->DeleteNews($idnews);
 
-        if($result > 0){
-            $this->response(array(
-                'result' => 'Successful Delete',
-                'affected_rows' => $result,
-                'status_code' => 200
-                ), REST_Controller::HTTP_OK
-            );
-        }
-        else{
-            $this->response(array(
-                'result' => 'Failed Delete',
-                'affected_rows' => $result,
-                'status_code' => 200
-                ), REST_Controller::HTTP_OK
-            );
-        }
+            if($result > 0){
+                $this->response(array(
+                    'result' => 'Successful Delete',
+                    'affected_rows' => $result,
+                    'status_code' => 200
+                    ), REST_Controller::HTTP_OK
+                );
+            }
+            else{
+                $this->response(array(
+                    'result' => 'Failed Delete',
+                    'affected_rows' => $result,
+                    'status_code' => 200
+                    ), REST_Controller::HTTP_OK
+                );
+            }
+		}
+		else {
+			$this->response(array('result' => 'No API KEY Provided'), 401);
+		}
     }
 }
--- application/controllers/api/user.php
+++ application/controllers/api/user.php
@@ -4,26 +4,31 @@ require APPPATH.'/libraries/REST_Controller.php';
 
 class USER extends REST_Controller
 {
-	function login_get()
-    {
-    	// $this->load->model('db_model');
-        $data = $this->db_model->CheckLoginData($this->get('username'), $this->get('password'));
-
-        if($data)
-            $this->response(array('result' => 'True'), 200); // 200 being the HTTP response code
-        else
-            $this->response(array('result' => 'Wrong Username or Password'), 200);
-    }
+	// function login_get()
+    // {
+    // 	// $this->load->model('db_model');
+    //     $data = $this->db_model->CheckLoginData($this->get('username'), $this->get('password'));
+	//
+    //     if($data)
+    //         $this->response(array('result' => 'True'), 200); // 200 being the HTTP response code
+    //     else
+    //         $this->response(array('result' => 'Wrong Username or Password'), 200);
+    // }
 
     function login_post()
     {
     	// $this->load->model('db_model');
-        $data = $this->db_model->CheckLoginData($this->post('username'), $this->post('password'));
+		if($this->api_model->CheckAPIKEY($this->post('api_key')) > 0){
+			$data = $this->db_model->CheckLoginData($this->post('username'), $this->post('password'));
 
-		if($data)
-            $this->response(array('result' => 'True'), 200); // 200 being the HTTP response code
-        else
-            $this->response(array('result' => 'Wrong Username or Password'), 200);
+			if($data)
+	            $this->response(array('result' => 'True'), 200); // 200 being the HTTP response code
+	        else
+	            $this->response(array('result' => 'Wrong Username or Password'), 200);
+		}
+		else {
+			$this->response(array('result' => 'No API KEY Provided'), 401);
+		}
     }
 
     function user_get()
@@ -33,126 +38,139 @@ class USER extends REST_Controller
 
     function user_post()
     {
-        $link = mysqli_connect("localhost", "root", "root", "plbtw");
+		if($this->api_model->CheckAPIKEY($this->post('api_key')) > 0){
+			$link = mysqli_connect("localhost", "root", "root", "plbtw");
 
-        $data = array(
-            'username' => mysqli_real_escape_string($link, $this->input->post('username')),
-            'password' => md5(mysqli_real_escape_string($link, $this->input->post('password'))),
-            'role' => strtolower(mysqli_real_escape_string($link, $this->input->post('roles')))
-        );
+	        $data = array(
+	            'username' => mysqli_real_escape_string($link, $this->input->post('username')),
+	            'password' => md5(mysqli_real_escape_string($link, $this->input->post('password'))),
+	            'role' => strtolower(mysqli_real_escape_string($link, $this->input->post('roles')))
+	        );
 
-		$user_check = $this->user_model->CheckUsername($data['username']);
+			$user_check = $this->user_model->CheckUsername($data['username']);
 
-		if($user_check > 0){
-			$this->response(array(
-				'result' => 'Username Is Registered',
-				'affected_rows' => $user_check,
-				), REST_Controller::HTTP_OK
-			);
+			if($user_check > 0){
+				$this->response(array(
+					'result' => 'Username Is Registered',
+					'affected_rows' => $user_check,
+					), REST_Controller::HTTP_OK
+				);
+			}
+			else{
+		        $result = $this->user_model->CreateUser($data);
+
+		        if($result > 0){
+					if($data['role'] == "developer"){
+						$md5 =  md5($data['username'].$data['password']);
+						$sha1 = sha1($md5);
+						$sha256 = hash('sha256', $sha1);
+						$sha512 = hash('sha512', $sha256);
+
+						$api_key_data = array(
+							'iduser' => $this->db->insert_id(),
+							'user_api_key' => $sha512
+					 	);
+
+						$api_key_result = $this->user_model->InsertAPIKEY($api_key_data);
+
+						if($api_key_result > 0){
+							$this->response(array(
+				                'result' => 'Successful Insertion',
+				                'affected_rows' => $api_key_result,
+								'api_key' => $sha512,
+				                ), REST_Controller::HTTP_OK
+				            );
+						}
+						else{
+				            $this->response(array(
+				                'result' => 'Failed Insertion',
+				                'affected_rows' => $result,
+				                ), REST_Controller::HTTP_OK
+				            );
+				        }
+					}
+
+		            $this->response(array(
+		                'result' => 'Successful Insertion',
+		                'affected_rows' => $result,
+		                ), REST_Controller::HTTP_OK
+		            );
+		        }
+		        else{
+		            $this->response(array(
+		                'result' => 'Failed Insertion',
+		                'affected_rows' => $result,
+		                ), REST_Controller::HTTP_OK
+		            );
+		        }
+			}
 		}
-		else{
-	        $result = $this->user_model->CreateUser($data);
+		else {
+			$this->response(array('result' => 'No API KEY Provided'), 401);
+		}
+    }
+
+    function user_put($iduser){
+		if($this->api_model->CheckAPIKEY($this->post('api_key')) > 0){
+			$link = mysqli_connect("localhost", "root", "root", "plbtw");
+
+	        parse_str(file_get_contents("php://input"),$put_vars);
+
+	        $data = array(
+	            'username' => $put_vars['username']
+	        );
+
+	        if(!empty($put_vars['password'])){
+	            $data['password'] = md5(mysqli_real_escape_string($link, $put_vars['username']));
+	        }
+
+	        if(!empty($put_vars['roles'])){
+	            $data['role'] = mysqli_real_escape_string($link, $put_vars['roles']);
+	        }
+
+	        $result = $this->user_model->UpdateUser($data, $iduser);
 
 	        if($result > 0){
-				if($data['role'] == "developer"){
-					$md5 =  md5($data['username'].$data['password']);
-					$sha1 = sha1($md5);
-					$sha256 = hash('sha256', $sha1);
-					$sha512 = hash('sha512', $sha256);
-
-					$api_key_data = array(
-						'iduser' => $this->db->insert_id(),
-						'user_api_key' => $sha512
-				 	);
-
-					$api_key_result = $this->user_model->InsertAPIKEY($api_key_data);
-
-					if($api_key_result > 0){
-						$this->response(array(
-			                'result' => 'Successful Insertion',
-			                'affected_rows' => $api_key_result,
-							'api_key' => $sha512,
-			                ), REST_Controller::HTTP_OK
-			            );
-					}
-					else{
-			            $this->response(array(
-			                'result' => 'Failed Insertion',
-			                'affected_rows' => $result,
-			                ), REST_Controller::HTTP_OK
-			            );
-			        }
-				}
-
 	            $this->response(array(
-	                'result' => 'Successful Insertion',
+	                'result' => 'Successful Update',
 	                'affected_rows' => $result,
 	                ), REST_Controller::HTTP_OK
 	            );
 	        }
 	        else{
 	            $this->response(array(
-	                'result' => 'Failed Insertion',
+	                'result' => 'Failed Update',
 	                'affected_rows' => $result,
 	                ), REST_Controller::HTTP_OK
 	            );
 	        }
 		}
-    }
-
-    function user_put($iduser){
-        $link = mysqli_connect("localhost", "root", "root", "plbtw");
-
-        parse_str(file_get_contents("php://input"),$put_vars);
-
-        $data = array(
-            'username' => $put_vars['username']
-        );
-
-        if(!empty($put_vars['password'])){
-            $data['password'] = md5(mysqli_real_escape_string($link, $put_vars['username']));
-        }
-
-        if(!empty($put_vars['roles'])){
-            $data['role'] = mysqli_real_escape_string($link, $put_vars['roles']);
-
-
-        }
-
-        $result = $this->user_model->UpdateUser($data, $iduser);
-
-        if($result > 0){
-            $this->response(array(
-                'result' => 'Successful Update',
-                'affected_rows' => $result,
-                ), REST_Controller::HTTP_OK
-            );
-        }
-        else{
-            $this->response(array(
-                'result' => 'Failed Update',
-                'affected_rows' => $result,
-                ), REST_Controller::HTTP_OK
-            );
-        }
+		else {
+			$this->response(array('result' => 'No API KEY Provided'), 401);
+		}
     }
 
     function user_delete($iduser){
-        $result = $this->user_model->DeleteUser($iduser);
+		if($this->api_model->CheckAPIKEY($this->post('api_key')) > 0){
+			$result = $this->user_model->DeleteUser($iduser);
 
-        if($result > 0){
-            $this->response(array(
-                'result' => 'Successful Delete',
-                'affected_rows' => $result,
-                ), REST_Controller::HTTP_OK
-            );
-        }
-        else{
-            $this->response(array(
-                'result' => 'Failed Delete',
-                'affected_rows' => $result,
-                ), REST_Controller::HTTP_OK
-            );
-        }
+	        if($result > 0){
+	            $this->response(array(
+	                'result' => 'Successful Delete',
+	                'affected_rows' => $result,
+	                ), REST_Controller::HTTP_OK
+	            );
+	        }
+	        else{
+	            $this->response(array(
+	                'result' => 'Failed Delete',
+	                'affected_rows' => $result,
+	                ), REST_Controller::HTTP_OK
+	            );
+	        }
+		}
+		else {
+			$this->response(array('result' => 'No API KEY Provided'), 401);
+		}
     }
 }
